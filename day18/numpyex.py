695fa3cfa3141cd034db2f292b59614def2c2556
puneetb97 <puneetb006@gmial.com>
2019-05-30 11:27:17 +0530

    use of numpy

---
+++ day18/numpyex.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+x = np.array([[[1,2,3],[4,5,6],[7,8,9]],[[11,22,33],[44,55,66],[77,88,99]],[[111,222,333],[444,555,666],[777,888,999]]])
+print(x)
+print(x.ndim)
+print(x.dtype)
+print(x.nbytes)
+print(x.size)
+print(x.shape)
+print(x.strides)
